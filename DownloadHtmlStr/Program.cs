b25f5d574debe410c794a671de466e9bac86735e
Софья Чернышова <sonya.chern@gmail.com>
2021-03-27 12:37:55 +0400

    I begin

---
+++ DownloadHtmlStr/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Web;
+
+namespace DownloadHtmlStr
+{
+    class Program
+    {
+        public static bool GetFile(string adress, string name)
+        {
+            WebClient client = new WebClient();
+            try
+            {
+                client.DownloadFile(adress,name);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
